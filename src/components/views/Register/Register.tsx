2bcfa1b4e5a6d5b8a3ebd41b3f32566407dc4d62
robbysuganda <robbyxsuganda@gmail.com>
2025-05-08 11:10:22 +0700

    feat: create ui register

---
+++ src/components/views/Register/Register.tsx
@@ -0,0 +1,102 @@
+import { Button, Card, CardBody, Input } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
+import useRegister from "./useRegister";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Register = () => {
+  const { visiblePassword, handleVisiblePassword } = useRegister();
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-10 lg:flex-row lg:gap-20">
+      <div className="flex w-full flex-col items-center justify-center gap-10 lg:w-1/3">
+        <Image
+          src={"/images/general/logo.svg"}
+          alt="logo"
+          width={180}
+          height={180}
+        />
+        <Image
+          src={"/images/illustrations/login.svg"}
+          alt="login"
+          width={1024}
+          height={1024}
+          className="w-2/3 lg:w-full"
+        />
+      </div>
+      <Card>
+        <CardBody className="p-8">
+          <h2 className="text-xl font-bold text-danger-500">Cerate Account</h2>
+          <p className="mb-4 text-small">
+            Have an account?&nbsp;
+            <Link href={"/login"} className="font-semibold text-danger-400">
+              Login here
+            </Link>
+          </p>
+          <form className="flex w-80 flex-col gap-4">
+            <Input
+              type="text"
+              label="Full Name"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type="text"
+              label="Username"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type="email"
+              label="Email"
+              variant="bordered"
+              autoComplete="off"
+            />
+            <Input
+              type={visiblePassword.password ? "text" : "password"}
+              label="Password"
+              variant="bordered"
+              autoComplete="off"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => handleVisiblePassword("password")}
+                >
+                  {visiblePassword.password ? (
+                    <FaEye className="pointer-events-none text-xl text-default-400" />
+                  ) : (
+                    <FaEyeSlash className="pointer-events-none text-xl text-default-400" />
+                  )}
+                </button>
+              }
+            />
+            <Input
+              type={visiblePassword.passwordConfirmation ? "text" : "password"}
+              label="Password Confirmation"
+              variant="bordered"
+              autoComplete="off"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => handleVisiblePassword("passwordConfirmation")}
+                >
+                  {visiblePassword.passwordConfirmation ? (
+                    <FaEye className="pointer-events-none text-xl text-default-400" />
+                  ) : (
+                    <FaEyeSlash className="pointer-events-none text-xl text-default-400" />
+                  )}
+                </button>
+              }
+            />
+            <Button color="danger" size="lg" type="submit">
+              Register
+            </Button>
+          </form>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@/styles/globals.css";
+import cn from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <main
+        className={cn(
+          inter.className,
+          "lg:py flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10",
+        )}
+      >
+        <Component {...pageProps} />
+      </main>
     </NextUIProvider>
   );
 }
